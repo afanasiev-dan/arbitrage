e7b117c3681a9b412a2e3614fb7143629c702f82
Daniil <arankart@gmail.com>
2025-08-18 23:21:34 +0300

    Ahahahahahahah

---
+++ Arbitrage.WebApi/Presentation/😀Controller.cs
@@ -0,0 +1,31 @@
+using Arbitrage.Domain;
+using Arbitrage.ExchangeDomain;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Arbitrage.WebApi.Presentation
+{
+    public class AhahahaController(
+        ILogger<AhahahaController> logger
+    ) : ControllerBase
+    {
+        private readonly ILogger<AhahahaController> _logger = logger;
+
+        [HttpPost("proxy")]
+        public async Task<IActionResult> Proxy([FromQuery] string url)
+        {
+            if (string.IsNullOrEmpty(url))
+                return BadRequest(new ApiResponce() { RetMsg = "Не указан URL" });
+
+            try
+            {
+                var response = await Network.PostAsync(url);
+                return Content(response, "application/json");
+            }
+            catch (HttpRequestException ex)
+            {
+                _logger.LogError(ex, "Ошибка при проксировании запроса");
+                return StatusCode(StatusCodes.Status500InternalServerError, new ApiResponce() { RetMsg = "Ошибка при проксировании запроса" });
+            }
+        }
+    }
+}
